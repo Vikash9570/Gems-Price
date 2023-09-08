a05ace4ff9b2d39c8792de79dc7111417eb4f416
Vikash <vikashprd30@gmail.com>
2023-09-08 20:52:54 +0530

    data transformation commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,7 +11,7 @@ from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path=os.path.join("arifacts","train.csv")
+    train_data_path=os.path.join("artifacts","train.csv")
     test_data_path=os.path.join("artifacts","test.csv")
     raw_data_path=os.path.join("artifacts","raw.csv")
 
@@ -34,7 +34,7 @@ class DataIngestion:
             train_set,test_set=train_test_split(df,test_size=0.33,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("ingestion of data completed")
 
@@ -46,6 +46,14 @@ class DataIngestion:
         except Exception as e:
             logging.info("Exception occured at data ingestion stage")
             raise CustomException(e,sys)
+        
+
+# checking our code running smoothly
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    obj_data_transformation=DataTransformation()
+    train_arr,test_arr,_=obj_data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
 
     
